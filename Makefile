51a6a1da7e31c575c915798a73280e8b7c3fd875
Marcus Noble <github@marcusnoble.co.uk>
2022-07-15 20:49:50 +0100

    Simplified to leverage standard Alpine image
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- Makefile
+++ Makefile
@@ -1,26 +1,5 @@
 .DEFAULT_GOAL := default
 
-IMAGE ?= averagemarcus/kube-ssh:latest
-
-export DOCKER_CLI_EXPERIMENTAL=enabled
-
-.PHONY: docker-build # Build the docker image
-docker-build:
-	@docker buildx create --use --name=crossplat --node=crossplat && \
-	docker buildx build \
-		--output "type=docker,push=false" \
-		--tag $(IMAGE) \
-		.
-
-.PHONY: docker-publish # Push the docker image to the remote registry
-docker-publish:
-	@docker buildx create --use --name=crossplat --node=crossplat && \
-	docker buildx build \
-		--platform linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/s390x \
-		--output "type=image,push=true" \
-		--tag $(IMAGE) \
-		.
-
 .PHONY: install # Installs the script to you bin directory
 install:
 	@cp ssh.sh /usr/local/bin/kube-ssh
--- ssh.sh
+++ ssh.sh
@@ -1,44 +1,70 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+NAMESPACE="$(kubectl config view --minify --output 'jsonpath={..namespace}' &>/dev/null)"
 set -e
+NAMESPACE=${NAMESPACE:-default}
+POD="kube-ssh"
+NODE=""
 
-NODES=$(kubectl get nodes --no-headers -o custom-columns=name:.metadata.name)
+print_usage() {
+  echo "kube-ssh - gain access to a Kubernetes host node (ssh-like for when a host doesn't have ssh)"
+  echo " "
+  echo "Usage:"
+  echo "kube-ssh [options]"
+  echo " "
+  echo "Options:"
+  echo "-h, --help            show this help text"
+  echo "-n, --namespace       the namespace to launch the pod in"
+  echo "-p, --pod             the name of the pod to launch (default: kube-ssh)"
+  echo "-N, --node            the name of the node to access"
+}
 
-if [ -z "$(which fzf)" ]; then
-  i=0
-  while read -r node; do
-    echo "[$i] - $node"
-    i=$((i+1))
-  done <<< "$NODES"
-  read -p "Which node would you like to connect to? " -r
-  echo ""
-  IFS=$'\n' NODES=($NODES)
-  NODE=${NODES[$REPLY]}
-else
-  NODE=$(echo "$NODES" | fzf)
+while test $# -gt 0; do
+  case "$1" in
+    -n|--namespace)
+      shift
+      NAMESPACE=$1
+      shift
+      ;;
+    -p|--pod)
+      shift
+      POD=$1
+      shift
+      ;;
+    -N|--node)
+      shift
+      NODE=$1
+      shift
+      ;;
+    -h|--help)
+      print_usage
+      exit 0
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+if [[ "$NODE" == "" ]]; then
+  NODES=$(kubectl get nodes --no-headers -o custom-columns=name:.metadata.name)
+
+  if [ -z "$(which fzf)" ]; then
+    i=0
+    while read -r node; do
+      echo "[$i] - $node"
+      i=$((i+1))
+    done <<< "$NODES"
+    read -p "Which node would you like to connect to? " -r
+    echo ""
+    IFS=$'\n' NODES=($NODES)
+    NODE=${NODES[$REPLY]}
+  else
+    NODE=$(echo "$NODES" | fzf)
+  fi
 fi
 
 NODE_NAME=$(kubectl get node $NODE -o template --template='{{index .metadata.labels "kubernetes.io/hostname"}}')
 NODE_SELECTOR='"nodeSelector": { "kubernetes.io/hostname": "'${NODE_NAME}'" },'
 
-kubectl run kube-ssh --restart=Never -it --rm --image overriden --overrides '
-{
-  "spec": {
-    "hostPID": true,
-    "hostNetwork": true,
-    '"${NODE_SELECTOR}"'
-    "tolerations": [{
-      "operator": "Exists"
-    }],
-    "containers": [
-      {
-        "name": "kube-ssh",
-        "image": "averagemarcus/kube-ssh:latest",
-        "stdin": true,
-        "tty": true,
-        "securityContext": {
-          "privileged": true
-        }
-      }
-    ]
-  }
-}' --attach "$@"
+kubectl run --namespace ${NAMESPACE} $POD --rm -it --image alpine --privileged --overrides '{"spec":{'"${NODE_SELECTOR}"'"hostPID": true}}' --command nsenter -- --mount=/proc/1/ns/mnt -- /bin/bash
